5b410d21b5db5e71ff2ca206d6dd45ae5e2779a1
Petar Blazevski <petar.blazevski@gmail.com>
2018-02-21 14:31:25 +0100

    Add ng2-dragula package for drag n drop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { DragulaModule } from 'ng2-dragula';
 
 
 import { AppComponent } from './app.component';
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DragulaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
